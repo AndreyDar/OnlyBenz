826875d169db9f3ea25681e66be57096ca2b768b
ge53ceh <andrii.dikhtiar@tum.de>
2024-04-28 01:13:16 +0200

    Antoniis function refactored, requirements.txt extended

---
+++ app/routers/chat_client_data_structure.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from ..gpt_datastructured_test import generate_description
+
+router = APIRouter()
+
+
+@router.post("/chat-structure/")
+async def chat_client_messages(raw_message):
+    car_recommendation = generate_description(raw_message)
+    return await {"car_recommendation": car_recommendation}
