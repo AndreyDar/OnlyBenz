b2ebbf24d6dfb75afcfdd27829f25dd6ae034b0f
Antonii Viter <viterantoniy20@gmail.com>
2024-04-28 04:44:28 +0200

    updated data structurer

--- app/gpt_datastructured_test.py
+++ app/gpt_datastructured_test.py
@@ -14,7 +14,7 @@ def generate_description(raw_message):
     As a Data Structurer, verify and extract the corresponding data to fill up (if data provided) the data structure expressed with the described bellow json format.
     This data comes from Mercedes-Benz customers and is related to Mercedes-Benz electric cars."""},
                 {"role": "system", "content": """
-    **MAIN TASK**: 1) PROVIDE TWO STRUCTURED JSON OUTPUTS ( for **FIRST TASK** and **SECOND TASK** ) THAT CORRESPOND TO EACH JSON FORMAT EXAMPLE TEMPLATES SPECIFIED IN FURTHER PROMPTS. 
+    **MAIN TASK**: 1) PROVIDE TWO STRUCTURED JSON OUTPUTS ( for **FIRST TASK** and **SECOND TASK** ) THAT CORRESPOND TO EACH JSON FORMAT EXAMPLE TEMPLATES SPECIFIED IN FURTHER PROMPTS. 2) PROVIDE A FLAG VERABLE, CALLED READY,(**FLAF VAR**) THAT SPECIFIES, THAT THERE IS ENOUGH DATA TO GIVE A RECOMENDATION. IT CAN HAVE EATHER VALUE true OR false.READY = true if is at least 50% of parameters are set.
     2) YOUR ANSWER MUST NOT CONTAIN ANY OTHER INFORMATION LIKE “YES, SURE” or "structure with VALUES, structure with WIGHTS" ETC., ONLY THIS TWO JSON STRUCRURES. 3) YOU MUST SPECIFY A CERTAIN VALUE TO EACH PARAMETER, NOT A RANGE. 5) PARAMETER'S VALUE ALLIGNED IN RANGE FORM 1 TO 10 DOES NOT CORRESPOND TO IT's WEIGHT 4) YOU CAN'T GO OUT OF RANGES AND MUST ALWAYS CONSIDER INFORMATION PROVIDED IN THE PARAMETER DESCRIPTION:
     """}, {"role": "system",
            "content": """
@@ -22,7 +22,6 @@ def generate_description(raw_message):
     1. Provide json structure with VALUES:
     a) Template:
     
-    //YOUR FIRST ANSWER START (DONT WRITE ANYTHING ELSE, other then the second json structure from the second answer)
     {
     “name": "Mercedes",
     "horsepower": 245,
@@ -38,13 +37,10 @@ def generate_description(raw_message):
     "globalRange": 20,
     "budget": 1000,
     }
-    //YOUR FIRST ANSWER END (DONT FORGET TO INCLUDE THE SECOND ANSWER)
-    b) Behevioure in case the user didn't specified some parameter's value and it doesn't emerges from the other:
+    b) Behevioure in case the user didn't specified some parameter's value AND it's value doesn't logically emerges from the other:
     For Boolean: value = false
-    For Numerical: value = {MEAN VALUE IN THE SPECIFIED INTERVAL}
+    For Numerical: value = None
     c) Parameter description:
-    
-    
     - name:
         1) Customer specified the parameter precisely:
             => value = specified value
@@ -58,76 +54,76 @@ def generate_description(raw_message):
             b) Customer wants average car: value is in range 300 - 400.
             c) Customer wants quite and economical car: value is in range 100 — 300.
     - PriceMin:
-    Range: [50000;150000]
+    Range: [40000;200000]
     Customer specified the parameter precisely:
     => value = specified value
     Customer specified the parameter, but not precisely:
-    a) Customer has a low minimum price preference: value is in range $50,000 - $75,000.
-    b) Customer has a medium minimum price preference: value is in range $75,001 - $100,000.
-    c) Customer has a high minimum price preference: value is in range $100,001 - $150,000.
+    a) Customer has a low minimum price preference: value is in range $40,000 - $75,000.
+    b) Customer has a medium minimum price preference: value is in range $75,001 - $120,000.
+    c) Customer has a high minimum price preference: value is in range $120,001 - $200,000.
     - PriceMax:
-    Range: [50000;150000]
+    Range: [40000;200000]
     Customer specified the parameter precisely:
     => value = specified value
     Customer specified the parameter, but not precisely:
-    a) Customer has a low maximum price preference: value is in range $50,000 - $75,000.
-    b) Customer has a medium maximum price preference: value is in range $75,001 - $100,000.
-    c) Customer has a high maximum price preference: value is in range $100,001 - $150,000.
+    a) Customer has a low maximum price preference: value is in range $40,000 - $75,000.
+    b) Customer has a medium maximum price preference: value is in range $75,001 - $120,000.
+    c) Customer has a high maximum price preference: value is in range $120,001 - $150,000.
     - Consumption:
-    Range: [10;60]
-    (How much kW/h does the car consume)
+    Range: [15000;23000]
+    (How much W/100km does the car consume)
     Customer specified the parameter precisely:
     => value = specified value
     Customer specified the parameter, but not precisely:
-    a) Customer prefers a highly efficient car: value is in range 10 - 20.
-    b) Customer prefers an average efficiency car: value is in range 30 - 40.
-    c) Customer prefers a less efficient car: value is in range 50 - 60.
+    a) Customer prefers a highly efficient car: value is in range 15000 - 17000.
+    b) Customer prefers an average efficiency car: value is in range 17000 - 20000.
+    c) Customer prefers a less efficient car: value is in range 20000 - 23000.
     - RangeIn:
-    Range: [100;1000]
+    Range: [300;760]
     (Range in town)
     Customer specified the parameter precisely:
     => value = specified value
     Customer specified the parameter, but not precisely:
-    a) Customer prefers a short-range car indoors: value is in range 100 - 400.
-    b) Customer prefers a medium-range car indoors: value is in range 401 - 700.
-    c) Customer prefers a long-range car indoors: value is in range 701 - 1000.
+    a) Customer prefers a short-range car indoors: value is in range 300 - 440.
+    b) Customer prefers a medium-range car indoors: value is in range 440 - 600.
+    c) Customer prefers a long-range car indoors: value is in range 600 - 760.
     - Range:
-    Range: [200;800]
+    Range: [270;750]
     (Range on the highways)
     Customer specified the parameter precisely:
     => value = specified value
     Customer specified the parameter, but not precisely:
-    a) Customer prefers a short-range car indoors: value is in range 200 - 300.
-    b) Customer prefers a medium-range car indoors: value is in range 350 - 500.
-    c) Customer prefers a long-range car indoors: value is in range 600 - 800.
+    a) Customer prefers a short-range car indoors: value is in range 270 - 380.
+    b) Customer prefers a medium-range car indoors: value is in range 380 - 560.
+    c) Customer prefers a long-range car indoors: value is in range 560 - 750.
     - Seats:
-    Range: [4;8]
+    Range: [4;10]
     Customer specified the parameter precisely:
     => value = specified value
     Customer specified the parameter, but not precisely:
     a) Customer wants a small car: value is in range 2 - 4 seats.
-    b) Customer wants a medium-sized car: value is in range 4 - 6 seats.
-    c) Customer wants a large car: value is in range 6 - 8 seats.
+    b) Customer wants a medium-sized car: value is in range 5 - 7 seats.
+    c) Customer wants a large car: value is in range 7 - 10 seats.
     - ChargeTime:
-    Range: [30;120]
+    Range: [30;45]
     Customer specified the parameter precisely:
     => value = specified value
     Customer specified the parameter, but not precisely:
-    a) Customer wants a fast-charging car: value is in range 30 - 40 minutes.
-    b) Customer wants an average-charging car: value is in range 41 - 60 minutes.
-    c) Customer wants a slow-charging car: value is in range 61 - 120 minutes.
+    a) Customer wants a fast-charging car: value is in range 30 - 35 minutes.
+    b) Customer wants an average-charging car: value is in range 35 - 40 minutes.
+    c) Customer wants a slow-charging car: value is in range 40 - 45 minutes.
     - Is4Matic:
     Customer specified the parameter:
     a) Customer prefers a car with 4MATIC: value is true.
     b) Customer prefers a car without 4MATIC: value is false.
-    c) Not defined => true if emerges from other requirements, otherwise false
+    c) Not defined => true if emerges from other requirements, otherwise None
     - Size:
-    Range: [1;10]
+    Range: [4;10]
     Customer specified the parameter precisely:
     => value = specified value
     Customer specified the parameter, but not precisely:
-    a) Customer wants a compact car: value is in range 1 - 2.
-    b) Customer wants a mid-size car: value is in range 3 - 4.
+    a) Customer wants a compact car: value is 4.
+    b) Customer wants a mid-size car: value is in range 5 - 7.
     c) Customer wants a big car: value is in range 8 - 10.
     - GlobalRange:
     Range: [1;10]
@@ -135,7 +131,7 @@ def generate_description(raw_message):
     Customer specified the parameter precisely:
     => value = specified value
     Customer specified the parameter, but not precisely:
-    a) Customer wants a car for a farm: value is in range 1- 3.
+    a) Customer wants a car for a farm: value is in range 1 - 3.
     b) Customer wants a car for a village: value is in range 4 - 6.
     c) Customer wants a car for a town: value is in range 7 - 10.
     - Budget:
@@ -143,8 +139,8 @@ def generate_description(raw_message):
     Customer specified the parameter precisely:
     => value = specified value
     Customer specified the parameter, but not precisely:
-    a) Customer has a low budget: value is in range 20000 - 50000.
-    b) Customer has a medium budget: value is in range 50000 - 90000.
+    a) Customer has a low budget: value is in range 40000 - 60000.
+    b) Customer has a medium budget: value is in range 60000 - 90000.
     c) Customer has a high budget: value is in range 90000 - 220000.
     """}, {"role": "system", "content": """
     **SECOND TASK**
@@ -158,7 +154,7 @@ def generate_description(raw_message):
         => weight is in range 3 - 7
     4. Customer didn’t mentioned the parameter, and it can not be emerged from other parameters
         => weight is in range 0 - 2
-    //YOUR SECOND ANSWER START (ALWAYS INCLUDE IT)
+    //YOUR SECOND ANSWER TEMPLATE START (ALWAYS INCLUDE IT)
     {
     “horsepower": 2,
     "priceMin": 1,
@@ -173,7 +169,15 @@ def generate_description(raw_message):
     "globalRange": 1,
     "budget": 2,
     }
-    //YOUR SECOND ANSWER END
+    """}, {"role": "system", "content": """
+    **FLAG VAR**
+    Flag var example:
+    //YOUR THIRD ANSWER EXAMPLE START
+    1)Consider 50% of parameters aren't set to None
+    ready = true
+    2)Consider 50% of parameters are set to None
+    ready = false
+
     """},
 
     {"role": "user", "content": "I'm in the market for an electric car that's sleek, fast, and has all the latest tech features. Price isn't a huge concern, but I don't want to break the bank. I need something with enough range to handle my daily commute and occasional road trips."},
@@ -183,13 +187,13 @@ configuration: {
 "horsepower": 400,
 "priceMin": 100000,
 "priceMax": 60000,
-"consumption": 15,
+"consumption": None,
 "range": 400,
 "rangeIn": 500,
-"seats": 4,
-"chargeTime": 45,
-"is4Matic": false,
-"size": 5,
+"seats": None,
+"chargeTime": None,
+"is4Matic": None,
+"size": None,
 "globalRange": 8,
 "budget": 70000
 }
@@ -197,41 +201,108 @@ weights: {
 "horsepower": 9,
 "priceMin": 6,
 "priceMax": 8,
-"consumption": 3,
+"consumption": 0,
 "range": 8,
 "rangeIn": 9,
-"seats": 3,
-"chargeTime": 5,
-"is4Matic": 2,
-"size": 6,
+"seats": 0,
+"chargeTime": 0,
+"is4Matic": 0,
+"size": 0,
 "globalRange": 7,
 "budget": 2
-}"""},
-{
-    "role" : "user", "content" : "I'm looking for an electric car that's compact, affordable, and eco-friendly. I live in the city, so I don't need a lot of horsepower or range. It should have enough space for groceries and a couple of passengers."
 }
-    #{"role": "user", "content": f"{raw_message}"},
-    #{"role": "assistant", "content": f"{raw_message}"},
+ready = true
+"""},
+    {"role": "user", "content": "I'm an eco-conscious consumer looking for an electric car with a focus on sustainability. It should have a minimal carbon footprint and be made from recycled materials where possible. Range and price are secondary considerations for me."
+},
+    {"role": "assistant", "content": """
+configuration: {
+"name": "Mercedes",
+"horsepower": None,
+"priceMin": None,
+"priceMax": None,
+"consumption": 20000,
+"range": None,
+"rangeIn": None,
+"seats": None,
+"chargeTime": None,
+"is4Matic": None,
+"size": None,
+"globalRange": None,
+"budget": None
+}
+weights: {
+"horsepower": 0,
+"priceMin": 0,
+"priceMax": 0,
+"consumption": 10,
+"range": 0,
+"rangeIn": 0,
+"seats": 0,
+"chargeTime": 0,
+"is4Matic": 0,
+"size": 0,
+"globalRange": 0,
+"budget": 0
+}
+ready = false"},
+"""},
+                {"role": "user",
+                 "content": "I'm a tech enthusiast looking for an electric car with the latest connectivity options and autonomous driving capabilities. Performance is important to me, so I want something with high horsepower and fast acceleration. Price is not a concern."},
+                {"role": "assistant", "content": """
+    configuration: {
+    "name": "Mercedes",
+    "horsepower": 600,
+    "priceMin": 200000,
+    "priceMax": 150000,
+    "consumption": None,
+    "range": None,
+    "rangeIn": None,
+    "seats": None,
+    "chargeTime": None,
+    "is4Matic": None,
+    "size": None,
+    "globalRange": None,
+    "budget": 200000
+    }
+    weights: {
+    "horsepower": 9,
+    "priceMin": 6,
+    "priceMax": 8,
+    "consumption": 0,
+    "range": 0,
+    "rangeIn": 0,
+    "seats": 0,
+    "chargeTime": 0,
+    "is4Matic": 0,
+    "size": 0,
+    "globalRange": 0,
+    "budget": 2
+    }
+    ready = false
+    """}
     #{"role": "user", "content": f"{raw_message}"},
     #{"role": "assistant", "content": f"{raw_message}"},
     #{"role": "user", "content": f"{raw_message}"}
 ]
+
+    messages.append({"role": "user", "content": f"{raw_message}"})
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages,
         temperature=0.1,
         max_tokens=250,
+        presence_penalty=1.5
     )
     reply = completion.choices[0].message.content
     return reply
 
 
 def main_test():
+
     print(generate_description(
-         "I'm an eco-conscious consumer looking for an electric car with a focus on sustainability. It should have a "
-         "minimal carbon footprint and be made from recycled materials where possible. Range and price are secondary "
-         "considerations for me."
+          "I'm in the market for an electric car that's sleek, fast, and has all the latest tech features. Price isn't a huge concern, but I don't want to break the bank. I need something with enough range to handle my daily commute and occasional road trips."
          )
     )
 
