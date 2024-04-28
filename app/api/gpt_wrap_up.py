7429ceb9bb4eb97d37924f7573c5a6d8fb161b86
Antonii Viter <viterantoniy20@gmail.com>
2024-04-28 08:31:30 +0200

    updated data wrap up

--- app/api/gpt_wrap_up.py
+++ app/api/gpt_wrap_up.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import json
 
 
-def generate_wrapup():
+def generate_wrapup(raw_message):
     with open('../../config/settings.json', 'r') as config_file:
         config = json.load(config_file)
 
@@ -13,50 +13,41 @@ def generate_wrapup():
                             You are wrapping a dealer chatbot conversation with mercades-benz customer.
                             Your Task:
                              Given a list of names of electric cars(next message provided by the system), that are being suggested to the user
-                              and given the names of cars chosen by the user(following message provided by the user) YOU NEED TO:
-                              1) Compare the GIVEN CARS and CHOSEN CARS
-                              2) Verify the metch (match, only if the at least one full name, i.e. every letter in it, matches)
-                              3) If there is a mathe:
+                             YOU NEED TO:
+                              1) Verify the match:
+                                    There is a match if the user explicitly specified the car name he wants. Otherwise there is no match
+                              2) If there is a match:
                                 You need to substitute the example values(**EXAMPLE**) of the parameters listed in a json structure(**TAMPLATE**). The instruction to the substitution of example values and parameter description will be provided with a json structure sample.
                                 **TEMPLATES**
                                 {
                                 "chosenCars" : [**EXAMPLE**, **EXAMPLE**,...]
                                 "anyMatches" : true
                                 }
-                              4) If there are no mathes: 
+                              3) If there are no mathes: 
                                 Set "anyMatches" with false: 
                                 {
                                 "anyMatches" : false
                                 }
-                            """}, {"role": "system",
-                 "content": """ 
-                            EQS, EQB
                             """},
                 {"role": "user",
                  "content": """ 
-                                No, that's not what i wanted
+                                No, that's not what I wanted
                                 """},
                 {"role": "assistant",
                  "content": """ 
                             {
                                 "anyMatches" : false
                             }
-                            """}, {"role": "system",
-                 "content": """ 
-                            EQB
                             """},
                 {"role": "user",
                  "content": """ 
-                                EQA 300
+                                Yes, I lowe this car!
                                 """},
                 {"role": "assistant",
                  "content": """ 
                             {
                                 "anyMatches" : false
                             }
-                            """}, {"role": "system",
-                 "content": """ 
-                            EQS
                             """},
                 {"role": "user",
                  "content": """ 
@@ -71,15 +62,16 @@ def generate_wrapup():
                             """}
                 ]
 
-    messages.append({"role" : "system", "content" : "EQA, EQS, EQB"})
-    messages.append({"role": "user", "content": "EQB"})
+    messages.append({"role": "user", "content": f"{raw_message}"})
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages,
-        temperature=1,
+        temperature=0.1,
         max_tokens=250,
     )
+
+
 
 
     reply = completion.choices[0].message.content
@@ -87,7 +79,7 @@ def generate_wrapup():
 
 
 def main_test_1():
-    print(generate_wrapup())
+    print(generate_wrapup("I love EQS"))
 
 
 # Press the green button in the gutter to run the script.
